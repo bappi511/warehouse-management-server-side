0331dea73862cc2541160ffab1708b4f0415df6e
bappi511 <bappyhossain018@gmail.com>
2022-05-22 13:35:39 +0600

    updatedProduct

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
 const express = require("express");
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjn2o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('electronicsWarehouse').collection('product');
+        const query = {};
+
+        app.get('/product', async (req, res) => {
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        });
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const products = await productCollection.findOne(query);
+            res.send(products);
+        });
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedItem = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedProduct = {
+
+                $set: {
+                    quantity: updatedItem.quantity
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedProduct, options);
+            res.send(result);
+
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
 //middleware;
 app.use(cors());
 app.use(express.json());
